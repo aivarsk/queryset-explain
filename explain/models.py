97f3975d05afdddb0b0809c2477a349f27f74cd7
Aivars Kalvans <aivars.kalvans@gmail.com>
2025-05-21 12:27:30 +0300

    forgot to add

--- explain/models.py
+++ explain/models.py
@@ -1,28 +1,69 @@
+import uuid
+
+from django.contrib.postgres.indexes import GinIndex, OpClass
 from django.db import models
+from django.db.models.functions import Cast, Upper
 
 
-class CardRange(models.Model):
-    start_range = models.CharField(max_length=19)
-    end_range = models.CharField(max_length=19)
-    # Just something to store along the range
-    info = models.CharField(max_length=64)
+class Member(models.Model):
+    uid = models.UUIDField(editable=False, unique=True, default=uuid.uuid4)
+    name = models.CharField(max_length=256, db_index=True)
+    external_id = models.CharField(max_length=64, db_index=True, blank=True, null=True)
 
     class Meta:
         indexes = [
             models.Index(
-                fields=["-start_range", "end_range"], name="card_range_start_end_idx"
+                Upper("name"),
+                name="name_upper_idx",
+            ),
+            GinIndex(
+                OpClass(Upper("name"), name="gin_trgm_ops"), name="name_search_idx"
+            ),
+            GinIndex(OpClass("name", name="gin_trgm_ops"), name="name_gin_idx"),
+            GinIndex(
+                OpClass(
+                    Upper(Cast("uid", output_field=models.TextField())),
+                    name="gin_trgm_ops",
+                ),
+                name="uid_search_idx",
+            ),
+            models.Index(
+                "name",
+                name="name_no_external_id_idx",
+                condition=models.Q(external_id__isnull=True),
             ),
         ]
 
-class FlatRange(models.Model):
-    pan = models.CharField(max_length=19)
-    klass = models.IntegerField()
-    # Just something to store along the range
-    info = models.CharField(max_length=64)
+
+class DocumentStatus(models.TextChoices):
+    UNKNOWN = "unknown", "Unknown"
+    VALID = "valid", "Valid"
+    INVALID = "invalid", "Invalid"
+
+
+class Document(models.Model):
+    member = models.ForeignKey(Member, models.CASCADE)
+    status = models.CharField(
+        max_length=64,
+        choices=DocumentStatus.choices,
+        default=DocumentStatus.UNKNOWN,
+        db_index=True,
+    )
+    data = models.JSONField(default=dict)
 
     class Meta:
         indexes = [
-            models.Index(
-                fields=["pan", "klass"], name="card_range_pan_idx"
+            models.Index(models.F("data__document_no"), name="data__document_no_idx"),
+            GinIndex(
+                OpClass(
+                    Upper(
+                        Cast(
+                            models.F("data__document_no"),
+                            output_field=models.TextField(),
+                        )
+                    ),
+                    name="gin_trgm_ops",
+                ),
+                name="data__document_no_search_idx",
             ),
         ]
